43d5c6597b0462098883ed25998f31d56e20ae6a
rbeketov <problemkarlfree@gmail.com>
2023-11-07 19:38:17 +0300

    without base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/metrostreams-frontend/',
+  // base: '/metrostreams-frontend/',
   plugins: [react()],
   server: {
     port: 3000,

180d6e22450c73b893d17cee19d0b0fe99d1fd11
rbeketov <problemkarlfree@gmail.com>
2023-11-07 23:24:05 +0300

    smart routing v6.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import ModelingsPage from './components/ModelingsPage.tsx';
 import ModelingsDetailsPage from './components/ModelingsDetailsPage.tsx';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 // import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 
-function RedirectComponent() {
-  const navigate = useNavigate();
-  React.useEffect(() => {
-    navigate('modelings/');
-  }, [navigate]);
+// function RedirectComponent() {
+//   const navigate = useNavigate();
+//   React.useEffect(() => {
+//     navigate('modelings/');
+//   }, [navigate]);
 
-  return null;
-}
+//   return null;
+// }
 
 const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/' : '/';
 
@@ -45,7 +45,7 @@ const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/' : '/';
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path={base_path} element={<RedirectComponent />}/>
+      <Route path={base_path} element={<ModelingsDetailsPage />}/>
       <Route path="modelings/" element={<ModelingsPage />}/>
       <Route path="modelings/:id/" element={<ModelingsDetailsPage />} />
     </Routes>

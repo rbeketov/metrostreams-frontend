97484eb2bf4e314753294cd3a552e0e0259499ae
rbeketov <problemkarlfree@gmail.com>
2023-11-08 01:09:43 +0300

    flexible routing v2.0

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,13 @@ import ModelingsDetailsPage from './components/ModelingsDetailsPage.tsx';
 import { useNavigate } from 'react-router-dom';
 // import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 
 
 const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/' : '/';
-// const FlexibleRouter = process.env.GITHUB_ACTIONS ? HashRouter : BrowserRouter
-// const prefix = process.env.GITHUB_ACTIONS ? '#/' : ''
+const FlexibleRouter = process.env.GITHUB_ACTIONS ? HashRouter : BrowserRouter;
 
 
 function RedirectComponent() {
@@ -23,14 +22,13 @@ function RedirectComponent() {
   return null;
 }
 
-
 ReactDOM.render(
-  <HashRouter>
+  <FlexibleRouter>
     <Routes>
       <Route path={base_path} element={<RedirectComponent />}/>
       <Route path="modelings/" element={<ModelingsPage />}/>
-      <Route path="modelings/" element={<ModelingsDetailsPage />} />
+      <Route path="modelings/:id/" element={<ModelingsDetailsPage />} />
     </Routes>
-  </HashRouter>,
+  </FlexibleRouter>,
   document.getElementById('root')
 );

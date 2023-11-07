4d9bb9f8668caefe2239cba370e1a8e42b563f62
rbeketov <problemkarlfree@gmail.com>
2023-11-08 00:58:04 +0300

    flexible routing v0.2

--- src/main.tsx
+++ src/main.tsx
@@ -8,39 +8,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 
+
+const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/' : '/';
+const FlexibleRouter = process.env.GITHUB_ACTIONS ? HashRouter : BrowserRouter
+// const prefix = process.env.GITHUB_ACTIONS ? '#/' : ''
+
+
 function RedirectComponent() {
   const navigate = useNavigate();
   React.useEffect(() => {
-    navigate('modelings/');
+    navigate("modelings/");
   }, [navigate]);
-
   return null;
 }
-
-const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/#/' : '/';
-const FlexibleRouter = process.env.GITHUB_ACTIONS ? HashRouter : BrowserRouter
-
-// const router = createBrowserRouter([
-//   {
-//     path: base_path,
-//     element: <RedirectComponent />,
-//   },
-//   {
-//     path: 'modelings/',
-//     element: <ModelingsPage />,
-//   },
-//   {
-//     path: 'modelings/:id/',
-//     element: <ModelingsDetailsPage />,
-//   },
-// ]);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 
 ReactDOM.render(
@@ -48,7 +28,7 @@ ReactDOM.render(
     <Routes>
       <Route path={base_path} element={<RedirectComponent />}/>
       <Route path="modelings/" element={<ModelingsPage />}/>
-      <Route path="modelings/:id/" element={<ModelingsDetailsPage />} />
+      <Route path="modelings/" element={<ModelingsDetailsPage />} />
     </Routes>
   </FlexibleRouter>,
   document.getElementById('root')

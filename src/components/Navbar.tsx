c102b78cb0334c42310ba40fd1f85cf328903f80
rbeketov <problemkarlfree@gmail.com>
2023-11-08 02:01:43 +0300

    flexible routing v5.0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
-import logoImage from '/cloud_splash.jpg';
+import logoImage from '/logo.png';
 import '../style/Navbar.css';
 
 function NavbarAnyMetro() {
@@ -13,7 +13,9 @@ function NavbarAnyMetro() {
             <Link to="/modelings/">
                 <Image src={logoImage} roundedCircle className="logo-img" alt="Логотип AnyMetro" />
             </Link>
-            <Navbar.Brand href="/modelings/" className="brand-text">AnyMetro</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/modelings/" className="brand-text">
+                AnyMetro
+            </Navbar.Brand>
             <Nav className="ms-auto">
                 <Link to="" className="btns-log">Зарегистрироваться</Link>
                 <Link to="" className="btns-log">Войти</Link>
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import ModelingsPage from './components/ModelingsPage.tsx';
 import ModelingsDetailsPage from './components/ModelingsDetailsPage.tsx';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {BrowserRouter} from "react-router-dom";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import './index.css';
 
-const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/' : '/';
-// const FlexibleRouter = process.env.GITHUB_ACTIONS ? HashRouter : BrowserRouter;
+const isLocal = process.env.NODE_ENV === 'development';
+
+const base_path = isLocal ? '/' : 'metrostreams-frontend/';
+
 
 function RedirectComponent() {
   const navigate = useNavigate();
@@ -19,13 +20,15 @@ function RedirectComponent() {
   return null;
 }
 
+const RouterComponent = isLocal ? BrowserRouter : HashRouter;
+
 ReactDOM.render(
-  <HashRouter>
+  <RouterComponent>
     <Routes>
       <Route path={base_path} element={<RedirectComponent />}/>
       <Route path="modelings/" element={<ModelingsPage />}/>
       <Route path="modelings/:id/" element={<ModelingsDetailsPage />} />
     </Routes>
-  </HashRouter>,
+  </RouterComponent>,
   document.getElementById('root')
 );

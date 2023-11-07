4a567f7db9592fe1e5a93a31f3cd7da64038b90b
rbeketov <problemkarlfree@gmail.com>
2023-11-08 01:46:41 +0300

    flexible routing v4.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
-
+import { Link } from 'react-router-dom';
+import logoImage from '/cloud_splash.jpg';
 import '../style/Navbar.css';
 
 function NavbarAnyMetro() {
     return (
         <Navbar className="color-navbar" expand="lg">
         <Container>
-            <a href="/modelings/">
-                <Image src="/logo.png" roundedCircle className="logo-img" alt="Логотип AnyMetro" />
-            </a>
+            <Link to="/modelings/">
+                <Image src={logoImage} roundedCircle className="logo-img" alt="Логотип AnyMetro" />
+            </Link>
             <Navbar.Brand href="/modelings/" className="brand-text">AnyMetro</Navbar.Brand>
             <Nav className="ms-auto">
-                <a href="" className="btns-log">Зарегистрироваться</a>
-                <a href="" className="btns-log">Войти</a>
+                <Link to="" className="btns-log">Зарегистрироваться</Link>
+                <Link to="" className="btns-log">Войти</Link>
             </Nav>
         </Container>
         </Navbar>

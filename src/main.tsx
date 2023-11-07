59dbacb3612b128c4fddc27d1b35035db303272a
rbeketov <problemkarlfree@gmail.com>
2023-11-07 22:09:51 +0300

    smart routing v5

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ModelingsPage from './components/ModelingsPage.tsx';
 import ModelingsDetailsPage from './components/ModelingsDetailsPage.tsx';
-import { createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 
 function RedirectComponent() {
@@ -17,24 +19,36 @@ function RedirectComponent() {
 
 const base_path = process.env.GITHUB_ACTIONS ? 'metrostreams-frontend/' : '/';
 
-const router = createBrowserRouter([
-  {
-    path: base_path,
-    element: <RedirectComponent />,
-  },
-  {
-    path: 'modelings/',
-    element: <ModelingsPage />,
-  },
-  {
-    path: 'modelings/:id/',
-    element: <ModelingsDetailsPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: base_path,
+//     element: <RedirectComponent />,
+//   },
+//   {
+//     path: 'modelings/',
+//     element: <ModelingsPage />,
+//   },
+//   {
+//     path: 'modelings/:id/',
+//     element: <ModelingsDetailsPage />,
+//   },
+// ]);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path={base_path} element={<RedirectComponent />}/>
+      <Route path="modelings/" element={<ModelingsPage />}/>
+      <Route path="modelings/:id/" element={<ModelingsDetailsPage />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
